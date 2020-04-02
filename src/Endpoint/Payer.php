b658ee9a01a38bbd3529ce0f45629df4fd6d775a
unknown <admin@79mplus.com>
2020-04-02 13:56:25 +0600

    code structure updated. added save payment source function in Payer endpoint

--- src/Endpoint/Payer.php
+++ src/Endpoint/Payer.php
@@ -26,17 +26,20 @@ class Payer extends Endpoint
      * @return mixed
      */
     public function add($email, $first_name, $last_name = '', $mobile = ''){
+        $_body = array(
+            'email'        => $email,
+            'firstName'    => $first_name,
+            'lastName'     => $last_name,
+            'mobileNumber' => $mobile
+        );
+        $_body_json              = json_encode( (object) $_body );
+
         $resp = $this->http_client->request('POST', $this->api_url. '/payers', [
             'headers' => [
                 'Content-Type'  => 'application/json',
                 'Authorization' => 'Bearer ' . $this->pinch->token
             ],
-            'body' => "{
-                    'email'          : '{$email}',
-                    'firstName'      : '{$first_name}',
-                    'lastName'       : '{$last_name}'
-                    'mobileNumber'    : '{$mobile}',
-                }"
+            'body' => $_body_json
         ]);
 
         return $resp->getBody()->getContents();
@@ -52,23 +55,54 @@ class Payer extends Endpoint
      */
 
     public function update($id, $email, $first_name, $last_name = '', $mobile = ''){
+        $_body = array(
+            'id'           => $id,
+            'email'        => $email,
+            'firstName'    => $first_name,
+            'lastName'     => $last_name,
+            'mobileNumber' => $mobile
+        );
+        $_body_json              = json_encode( (object) $_body );
+
         $resp = $this->http_client->request('POST', $this->api_url. '/payers', [
             'headers' => [
                 'Content-Type'  => 'application/json',
                 'Authorization' => 'Bearer ' . $this->pinch->token
             ],
-            'body' => "{
-                    'id'             : '{$id}',
-                    'email'          : '{$email}',
-                    'firstName'      : '{$first_name}',
-                    'lastName'       : '{$last_name}'
-                    'mobileNumber'   : '{$mobile}',
-                }"
+            'body' => $_body_json
         ]);
 
         return $resp->getBody()->getContents();
     }
 
+    /**
+     * @param  string $payer_id
+     * @param  string $source_type
+     * @param  string $credit_card_token
+     * @param  string $bank_account_name
+     * @param  string $bank_account_bsb
+     * @param  string $bank_account_number
+     * @return mixed
+     */
+    public function save_payment_source( $payer_id, $source_type, $credit_card_token, $bank_account_name = NULL, $bank_account_bsb  = NULL, $bank_account_number = NULL ) {
+        $_body = array(
+            'SourceType'        => $source_type,
+            'BankAccountName'   => $bank_account_name,
+            'BankAccountBsb'    => $bank_account_bsb,
+            'BankAccountNumber' => $bank_account_number,
+            'CreditCardToken'   => $credit_card_token
+        );
+        $_body_json              = json_encode( (object) $_body );
 
+        $resp = $this->http_client->request('POST', $this->api_url. '/payers/' . $payer_id . '/sources', [
+            'headers' => [
+                'Content-Type'  => 'application/json',
+                'Authorization' => 'Bearer ' . $this->pinch->token
+            ],
+            'body' => $_body_json
+        ]);
+
+        return $resp->getBody()->getContents();
+    }
 
 }
--- src/Endpoint/Payment.php
+++ src/Endpoint/Payment.php
@@ -6,7 +6,7 @@ use GuzzleHttp\Client as HttpClient;
 
 class Payment extends Endpoint
 {
-    private $token;
+    private $token = NULL;
     private $api_url;
     private $http_client;
 
@@ -33,31 +33,8 @@ class Payment extends Endpoint
      * @return bool
      */
     public function execute($publishable_key, $card_no, $cvc, $expiry_month, $expiry_year, $card_holder_name, $email, $amount, $description = ''){
-        /*getting credit card token*/
-        // Tokens
-        $token_body = array(
-            'PublishableKey' => $publishable_key,
-            'cardNumber' => $card_no,
-            'cvc' => $cvc,
-            'expiryMonth' => $expiry_month,
-            'expiryYear' => $expiry_year,
-            'cardHolderName' => $card_holder_name
-        );
-        $token_body_json              = json_encode( (object) $token_body );
-
-        $resp = $this->http_client->request('POST', $this->api_url . '/tokens', [
-            'headers' => [
-                'Content-Type' => 'application/json',
-            ],
-            'body' => $token_body_json
-        ]);
-
-        if($resp->getStatusCode() == 200 || $resp->getStatusCode() == 201){
-            $json = json_decode($resp->getBody()->getContents());
-            if($json->token){
-                $this->token = $json->token;
-            }
-        }
+        /*set credit card token*/
+        $this->credit_card_token($publishable_key, $card_no, $cvc, $expiry_month, $expiry_year, $card_holder_name);
 
         if( $this->token ){
             $resp = $this->http_client->request('POST', $this->api_url. '/payments/realtime', [
@@ -102,6 +79,73 @@ class Payment extends Endpoint
         ]);
 
         return $resp->getBody()->getContents();
+    }
+
+    /**
+     * @param $payer_id
+     * @param $credit_card_token
+     * @param $source_id
+     * @param $amount
+     * @param $description
+     * @return mixed
+     */
+    public function realtime( $payer_id, $credit_card_token = '', $source_id = '', $amount, $description = ''){
+        /*Execute real-time payment*/
+        $_body = array(
+            'payerId'     => $payer_id,
+            'amount'      => $amount,
+            'description' => $description,
+        );
+
+        if( $credit_card_token !== '' ){
+            $_body['creditCardToken'] = $credit_card_token;
+        }
+
+        if( $source_id !== '' ){
+            $_body['sourceId'] = $source_id;
+        }
+
+        $_body_json     = json_encode( (object) $_body );
+
+        $resp = $this->http_client->request('POST', $this->api_url. '/payments/realtime', [
+            'headers' => [
+                'Content-Type'  => 'application/json',
+                'Authorization' => 'Bearer ' . $this->pinch->token
+            ],
+            'body' => $_body_json
+        ]);
+
+        return $resp->getBody()->getContents();
+    }
+
+    public function credit_card_token( $publishable_key, $card_no, $cvc, $expiry_month, $expiry_year, $card_holder_name ){
+        /*getting credit card token*/
+        $_body = array(
+            'PublishableKey' => $publishable_key,
+            'cardNumber'     => $card_no,
+            'cvc'            => $cvc,
+            'expiryMonth'    => $expiry_month,
+            'expiryYear'     => $expiry_year,
+            'cardHolderName' => $card_holder_name
+        );
+
+        $_body_json     = json_encode( (object) $_body );
+
+        $resp = $this->http_client->request('POST', $this->api_url . '/tokens', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'body' => $_body_json
+        ]);
+
+        if($resp->getStatusCode() == 200 || $resp->getStatusCode() == 201){
+            $json = json_decode($resp->getBody()->getContents());
+            if($json->token){
+                $this->token = $json->token;
+            }
+        }
+
+        return $this->token;
     }
 
 }
